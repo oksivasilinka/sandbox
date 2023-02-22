b679a9fc636b5bcc1c4d43d0be61d00ca18dda88
Oksana <oksana.s.kovalchuk@gmail.com>
2023-02-22 20:20:51 +0300

    21.02

---
+++ freeCodeCamp.js
@@ -0,0 +1,19 @@
+//a firstccomment
+/*a second
+comment */
+
+var myName;
+
+// Setup
+var a;
+a = 7;
+
+// Only change code below this line
+
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+
+// Only change code below this line

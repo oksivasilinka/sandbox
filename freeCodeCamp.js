1023372b732504819af3e5ce75b4dad91f9c8813
Oksana <oksana.s.kovalchuk@gmail.com>
2023-02-23 21:34:02 +0300

    23/02

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -23,3 +23,27 @@ var a = 9;
 
 var myFirstName = "Oksana";
 var myLastName = "Kovalchuk";
+
+
+// Only change code below this line
+var a = 5;
+var b=10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";

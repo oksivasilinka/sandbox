f1890dc64bddce13f434c46517088b99a3413398
Oksana <oksana.s.kovalchuk@gmail.com>
2023-02-24 19:23:06 +0300

    24/02

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -123,3 +123,5 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Ch
 const myStr1 = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 const myStr2 = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+const myStr3 = "This is the start. " + "This is the end."; // Change this line

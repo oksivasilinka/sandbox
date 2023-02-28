bee20c5e8204fbabe3099a9baa133345785ecc1e
Oksana <oksana.s.kovalchuk@gmail.com>
2023-02-28 21:49:40 +0300

    28/02

---
+++ FreeCodeCampJS/Basic JavaScript/91.js
@@ -0,0 +1,12 @@
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  obj.hasOwnProperty(checkProp);
+  if (true) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+  
+  
+  // Only change code above this line
+}

da00f24985066b5764290259838e2b40aad907a3
Oksana <oksana.s.kovalchuk@gmail.com>
2023-03-01 21:01:03 +0300

    01/03

---
+++ FreeCodeCampJS/Basic JavaScript/105.js
@@ -0,0 +1,6 @@
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  let number = Math.floor(Math.random() * (myMax - myMin +1)) + myMin;
+  return number;
+  // Only change code above this line
+}

2410e9c32ff427ee2cde7992850906c0dd5b6784
Oksana <oksana.s.kovalchuk@gmail.com>
2023-03-07 17:12:43 +0300

    07/03

---
+++ CodeWars/Do you speak English.js
@@ -0,0 +1,9 @@
+function spEng(sentence){
+    //write your code here
+      let lower = sentence.toLowerCase();
+      if(lower.indexOf('english') + 1) {
+        return true;
+      } else {
+        return false;
+      }
+    }

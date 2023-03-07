3f60c6483b19ace873437d9f7f29c4b4ee6ba728
Oksana <oksana.s.kovalchuk@gmail.com>
2023-03-07 22:30:14 +0300

    07/03

---
+++ CodeWars/Return the day.js
@@ -0,0 +1,11 @@
+function whatday(num) { 
+    // put your code here
+    return (num === 1) ? 'Sunday'
+    : (num === 2) ? 'Monday'
+    : (num === 3) ? 'Tuesday'
+    : (num === 4) ? 'Wednesday'
+    : (num === 5) ? 'Thursday'
+    : (num === 6) ? 'Friday'
+    : (num === 7) ? 'Saturday'
+    : 'Wrong, please enter a number between 1 and 7'; 
+  }

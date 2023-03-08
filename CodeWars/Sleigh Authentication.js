72faa01ac9911e3c9fa5e4439e590852f57be28b
Oksana <oksana.s.kovalchuk@gmail.com>
2023-03-08 12:36:16 +0300

    08/03

---
+++ CodeWars/Sleigh Authentication.js
@@ -0,0 +1,10 @@
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  // TODO
+  if (name === "Santa Claus" && password === "Ho Ho Ho!") {
+    return true;
+  } else {
+    return false;
+  }
+};
